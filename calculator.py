800d1cf8daf656472725635407b157dd273c8508
Pooneh-Bathaei2001 <148288173+Pooneh-Bathaei2001@users.noreply.github.com>
2023-10-24 17:02:57 +0330

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,45 @@
+op= input("enter op(+, -, *, /, sin, cos, tan, cot, factorial, sqrt):")
+import math
+if op == "+":
+    a = float(input("Enter a:"))
+    b = float(input("Enter b:"))
+    r = a + b 
+if op == "-":
+    a = float(input("Enter a:"))
+    b = float(input("Enter b:"))
+    r = a - b
+if op == "*":
+    a = float(input("Enter a:"))
+    b = float(input("Enter b:"))
+    r = a * b 
+if op == "/": 
+    a = float(input("Enter a:"))
+    b = float(input("Enter b:"))
+    if b == 0:
+        r = "Error"
+    else:
+        r = a / b
+
+if op == "sin": 
+    a = float(input("Enter a:"))
+    r = math.sin(math.radians(a))
+
+if op == "cos":
+    a = float(input("Enter a:"))
+    r = math.cos(math.radians(a))
+
+if op == "tan":
+    a = int(input("Enter a:"))
+    if math.cos(math.radians(a)) == 0:
+        print("Error")
+    else: 
+        r = math.tan(a)
+
+if op == "cot":
+    a = int(input("Enter a:"))
+    if math.sin(math.radians(a)) == 0:
+        print("Error")
+    else: 
+        r = 1/math.tan(math.radians(a))
+
+print(r)
