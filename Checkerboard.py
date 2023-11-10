45ab510c41b8506e4a943b5dcfa517fec4d85316
Pooneh-Bathaei2001 <148288173+Pooneh-Bathaei2001@users.noreply.github.com>
2023-11-10 17:42:48 +0330

    Delete Checkerboard.py

--- Checkerboard.py
+++
@@ -1,8 +0,0 @@
-
-n = int(input("Enter the length of your board:"))
-m = int(input("Enter the length of your board:"))
-
-for i in range(n):
-        for k in range(m):
-            print("#*", end = "")
-        print()
